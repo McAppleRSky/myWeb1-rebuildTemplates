6bdf8b70114aca0354ab486e2dfe2bdfc076229d
McAppleRSky <user0@DESKTOP-2AVECJ5.localdomain>
2020-08-05 00:06:37 +0300

    add subproject property file

--- src/main/javascript/prepare.js
+++ src/main/javascript/prepare.js
@@ -15,11 +15,15 @@ function getFile(name) {
     var chunk;
     var FileReader = Java.type("java.io.FileReader");
     var fileReader = new FileReader(name);
-    chunk = fileReader.read();
-    while (chunk != -1) {
-        data += String.fromCharCode(chunk);
+    do{
         chunk = fileReader.read();
-    }
+        data += String.fromCharCode(chunk);
+    }while (chunk != -1)
+//    chunk = fileReader.read();
+//    while (chunk != -1) {
+//        data += String.fromCharCode(chunk);
+//        chunk = fileReader.read();
+//    }
     fileReader.close();
     return data;
 }
